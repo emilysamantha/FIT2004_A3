30d69abcd8683f1c4cc9ec9964cc8fc942da7aea
emilysamantha <emilyzarry@gmail.com>
2022-10-14 23:16:54 +1100

    Solve task 1

--- assignment3.py
+++ assignment3.py
@@ -40,14 +40,237 @@ def allocate(availability):
 
     :Approach:
     """
-    graph = generate_network(availability)
-    breakfast, dinner = ford_fulkerson(availability, graph, 0, len(graph) - 1, len(availability))
+    # graph = generate_network(availability)
+    # breakfast, dinner, max_flow = ford_fulkerson(availability, graph, 0, len(graph) - 1, len(availability))
 
-    # If a day cannot be filled, then there is no valid allocation and return None
-    if -1 in breakfast or -1 in dinner:
+    # GENERAL INFORMATION
+    num_persons = 5
+    n = len(availability)  # n is the number of days
+
+    # REQUIREMENTS
+    min_meals = math.floor(0.36 * n)
+    max_meals = math.ceil(0.44 * n)
+    demand_min_flow = min_meals * num_persons             # represents the minimum meals to be prepared by all roommates
+    demand_adjusted = (n * 2) - (min_meals * num_persons) # represents the rest of meals to be prepared
+
+    graph_min_flow = generate_network_min_flow(availability)
+    breakfast_min_flow, dinner_min_flow, max_flow_min = ford_fulkerson(availability, graph_min_flow, 0,
+                                                                       len(graph_min_flow) - 1, len(availability))
+
+    graph_adjusted = generate_network_adjusted(availability, breakfast_min_flow, dinner_min_flow)
+    breakfast_adjusted, dinner_adjusted, max_flow_adjusted = ford_fulkerson(availability, graph_adjusted, 0,
+                                                                        len(graph_adjusted) - 1, len(availability))
+
+    # Check if the outgoing edges from the source are saturated in both graph_min_flow and graph_adjusted
+    if max_flow_min < demand_min_flow or max_flow_adjusted < demand_adjusted:
+        # Then it means no valid allocation exists and return None
         return None
 
+    # Merge breakfast_min_flow and breakfast_adjusted into final breakfast array
+    breakfast = []
+    for i in range(n):
+        if breakfast_min_flow[i] > -1:
+            breakfast.append(breakfast_min_flow[i])
+        elif breakfast_adjusted[i] > -1:
+            breakfast.append(breakfast_adjusted[i])
+
+    # Merge dinner_min_flow and dinner_adjusted into final dinner array
+    dinner = []
+    for i in range(n):
+        if dinner_min_flow[i] > -1:
+            dinner.append(dinner_min_flow[i])
+        elif dinner_adjusted[i] > -1:
+            dinner.append(dinner_adjusted[i])
+
     return breakfast, dinner
+
+
+def generate_network_min_flow(availability):
+    # GENERAL INFORMATION
+    num_persons = 5
+    n = len(availability)   # n is the number of days
+
+    # REQUIREMENTS
+    min_meals = math.floor(0.36 * n)
+    max_meals = math.ceil(0.44 * n)
+    max_order = math.ceil(0.1 * n)
+    demand = min_meals * num_persons  # represents the minimum meals to be prepared by all roommates
+
+    # NODE INFORMATION
+    source_node = 0
+    meals_node = 1
+    start_person_nodes = 2
+    order_option_node = 7
+    start_day_nodes = 8
+    start_meal_nodes = start_day_nodes + n
+    num_nodes = 1 + 1 + (num_persons + 1) + (n * 3) + 1
+    # source + meals + (num of people + order option) + (num of days * 3) + sink
+
+    # Generate empty graph
+    graph = [[0 for _ in range(num_nodes)] for _ in range(num_nodes)]
+
+    # Fill graph
+    # Edge from source node to meals node
+    graph[source_node][meals_node] = demand
+
+    # Edge from meals to each person (with capacity equal to the lower bound)
+    for i in range(num_persons):
+        graph[meals_node][i + start_person_nodes] = min_meals
+
+    # No edge from meals to order option (Not required)
+
+    # No edge from order option to every meal (Not required)
+
+    # Edge from each person to each day they can prepare a meal
+    # and edge from each day to each meal
+    for day in range(n):
+        for person in range(num_persons):
+            # If the person can prepare breakfast for that day
+            if availability[day][person] == 1:
+                # Add edge from person to the day
+                # Capacity is one since each person can only prepare one meal for each day
+                graph[person + start_person_nodes][day + start_day_nodes] = 1
+                # Add edge from the day to the corresponding meal of the day (i.e. breakfast)
+                graph[day + start_day_nodes][(day * 2) + start_meal_nodes] = 1
+
+            # If the person can prepare dinner for that day
+            elif availability[day][person] == 2:
+                # Add edge from person to the day
+                graph[person + start_person_nodes][day + start_day_nodes] = 1
+                # Add edge from the day to the corresponding meal of the day (i.e. dinner)
+                graph[day + start_day_nodes][(day * 2) + start_meal_nodes + 1] = 1
+
+            # If the person can prepare breakfast and dinner for that day
+            elif availability[day][person] == 3:
+                # Add edge from person to the day
+                graph[person + start_person_nodes][day + start_day_nodes] = 1
+                # Add edge from the day to the corresponding meal of the day (i.e
+                # Add edge to both breakfast and dinner for the day
+                graph[day + start_day_nodes][(day * 2) + start_meal_nodes] = 1
+                graph[day + start_day_nodes][(day * 2) + start_meal_nodes + 1] = 1
+
+    # Edge from each meal to super sink node
+    for i in range(n * 2):
+        graph[i + start_meal_nodes][num_nodes - 1] = 1
+
+    return graph
+
+
+def generate_network_adjusted(availability, breakfast_min_flow, dinner_min_flow):
+    # GENERAL INFORMATION
+    num_persons = 5
+    n = len(availability)  # n is the number of days
+
+    # REQUIREMENTS
+    min_meals = math.floor(0.36 * n)
+    max_meals = math.ceil(0.44 * n)
+    max_order = math.ceil(0.1 * n)
+    demand = (n * 2) - (min_meals * num_persons)  # represents the rest of meals to be prepared
+
+    # NODE INFORMATION
+    source_node = 0
+    meals_node = 1
+    start_person_nodes = 2
+    order_option_node = 7
+    start_day_nodes = 8
+    start_meal_nodes = start_day_nodes + n
+    num_nodes = 1 + 1 + (num_persons + 1) + (n * 3) + 1
+    # source + meals + (num of people + order option) + (num of days * 3) + sink
+
+    # Generate empty graph
+    graph = [[0 for _ in range(num_nodes)] for _ in range(num_nodes)]
+
+    # Fill graph
+    # Edge from source node to meals node
+    graph[source_node][meals_node] = demand
+
+    # Edge from meals to each person (with capacity equal to the remainder of meals possible for the person)
+    for i in range(num_persons):
+        graph[meals_node][i + start_person_nodes] = max_meals - min_meals
+
+    # Edge from meals to order option
+    graph[meals_node][order_option_node] = max_order
+
+    # Edge from order option to every meal
+    # Taking into account the allocated meals given by breakfast_min_flow and dinner_min_flow
+    # Only make an edge if that meal is not allocated a person yet
+    for meal in range(n * 2):
+        day = meal // 2
+        # If meal is even, it is a breakfast node
+        if meal % 2 == 0:
+            # If the breakfast for that day has been allocated, do not add an edge
+            if breakfast_min_flow[day] != -1:
+                continue
+        # If meal is odd, it is a dinner node
+        else:
+            # If the dinner for that day has been allocated, do not add an edge
+            if dinner_min_flow[day] != -1:
+                continue
+
+        graph[order_option_node][meal + start_meal_nodes] = 1
+
+        # Edge from the meal to sink node
+        graph[meal + start_meal_nodes][num_nodes - 1] = 1
+
+    # Edge from each person to each day they can prepare a meal
+    # and edge from each day to each meal
+    # Taking into account the allocated meals given by breakfast_min_flow and dinner_min_flow
+    # Only make an edge if that meal is not allocated a person yet
+    for day in range(n):
+        for person in range(num_persons):
+            # If the person can prepare breakfast for that day and breakfast for that day is not allocated yet
+            if availability[day][person] == 1 and breakfast_min_flow[day] == -1:
+                # Add edge from person to the day
+                # Capacity is one since each person can only prepare one meal for each day
+                graph[person + start_person_nodes][day + start_day_nodes] = 1
+                # Add edge from the day to the corresponding meal of the day (i.e. breakfast)
+                graph[day + start_day_nodes][(day * 2) + start_meal_nodes] = 1
+
+            # If the person can prepare dinner for that day and dinner for that day is not allocated yet
+            elif availability[day][person] == 2 and dinner_min_flow[day] == -1:
+                # Add edge from person to the day
+                graph[person + start_person_nodes][day + start_day_nodes] = 1
+                # Add edge from the day to the corresponding meal of the day (i.e. dinner)
+                graph[day + start_day_nodes][(day * 2) + start_meal_nodes + 1] = 1
+                # Edge from the meal to super sink node
+                graph[(day * 2) + start_meal_nodes + 1][num_nodes - 1] = 1
+
+            # If the person can prepare breakfast and dinner for that day
+            # and only dinner has been allocated (breakfast is not allocated yet)
+            elif availability[day][person] == 3 and dinner_min_flow[day] > -1 and breakfast_min_flow[day] == -1\
+                    and dinner_min_flow[day] != person:
+                # Add edge from person to the day
+                graph[person + start_person_nodes][day + start_day_nodes] = 1
+                # Add edge from the day to the corresponding meal of the day (i.e. breakfast)
+                graph[day + start_day_nodes][(day * 2) + start_meal_nodes] = 1
+                # Edge from the meal to super sink node
+                graph[(day * 2) + start_meal_nodes][num_nodes - 1] = 1
+
+            # If the person can prepare breakfast and dinner for that day
+            # and only breakfast has been allocated (dinner is not allocated yet)
+            elif availability[day][person] == 3 and breakfast_min_flow[day] > -1 and dinner_min_flow[day] == -1\
+                    and breakfast_min_flow[day] != person:
+                # Add edge from person to the day
+                graph[person + start_person_nodes][day + start_day_nodes] = 1
+                # Add edge from the day to the corresponding meal of the day (i.e. dinner)
+                graph[day + start_day_nodes][(day * 2) + start_meal_nodes + 1] = 1
+                # Edge from the meal to super sink node
+                graph[(day * 2) + start_meal_nodes + 1][num_nodes - 1] = 1
+
+            # If the person can prepare breakfast and dinner for that day
+            # and both breakfast and dinner is not allocated yet
+            elif availability[day][person] == 3 and breakfast_min_flow[day] == -1 and dinner_min_flow[day] == -1:
+                # Add edge from person to the day
+                graph[person + start_person_nodes][day + start_day_nodes] = 1
+                # Add edge from the day to the corresponding meal of the day (i.e. breakfast and dinner)
+                # Add edge to both breakfast and dinner for the day
+                graph[day + start_day_nodes][(day * 2) + start_meal_nodes] = 1
+                graph[day + start_day_nodes][(day * 2) + start_meal_nodes + 1] = 1
+                # Edge from the meals to super sink node
+                graph[(day * 2) + start_meal_nodes][num_nodes - 1] = 1          # breakfast to sink
+                graph[(day * 2) + start_meal_nodes + 1][num_nodes - 1] = 1      # dinner to sink
+
+    return graph
 
 
 def generate_network(availability):
@@ -55,8 +278,6 @@ def generate_network(availability):
     num_persons = 5
     n = len(availability)       # n is the number of days
     demand = n * 2              # represents the number of meals to be prepared (num of days * 2)
-    num_nodes = 1 + 1 + (num_persons + 1) + (n * 3) + 1
-    # source + meals + (num of people + order option) + (num of days * 3) + sink
 
     # REQUIREMENTS
     min_meals = math.floor(0.36 * n)
@@ -64,24 +285,28 @@ def generate_network(availability):
     max_order = math.ceil(0.1 * n)
 
     # NODE INFORMATION
+    source_node = 0
+    meals_node = 1
     start_person_nodes = 2
     order_option_node = 7
     start_day_nodes = 8
     start_meal_nodes = start_day_nodes + n
+    num_nodes = 1 + 1 + (num_persons + 1) + (n * 3) + 1
+    # source + meals + (num of people + order option) + (num of days * 3) + sink
 
     # Generate empty graph
     graph = [[0 for _ in range(num_nodes)] for _ in range(num_nodes)]
 
     # Fill graph
-    # Edge from source node (0) to meals (1)
-    graph[0][1] = demand
+    # Edge from source node to meals node
+    graph[source_node][meals_node] = demand
 
     # Edge from meals to each person
     for i in range(num_persons):
-        graph[1][i + 2] = max_meals
+        graph[meals_node][i + start_person_nodes] = max_meals
 
     # Edge from meals to order option
-    graph[1][7] = max_order
+    graph[meals_node][order_option_node] = max_order
 
     # Edge from order option to every meal
     for meal in range(n * 2):
@@ -110,7 +335,7 @@ def generate_network(availability):
             elif availability[day][person] == 3:
                 # Add edge from person to the day
                 graph[person + start_person_nodes][day + start_day_nodes] = 1
-                # Add edge from the day to the corresponding meal of the day (i.e
+                # Add edge from the day to the corresponding meal of the day (i.e. breakfast and dinner)
                 # Add edge to both breakfast and dinner for the day
                 graph[day + start_day_nodes][(day * 2) + start_meal_nodes] = 1
                 graph[day + start_day_nodes][(day * 2) + start_meal_nodes + 1] = 1
@@ -157,10 +382,11 @@ def bfs(availability, network, source, sink, parent, n):
                     person = parent[parent[curr]] - start_person_nodes
 
                     # If the person is not the order option node
-                    if person != 5:
+                    if parent[curr] != 7:
                         # If curr is a breakfast node and the person can only prepare dinner or
                         # if curr is a dinner node and the person can only prepare breakfast
-                        if (curr % 2 == 0 and (availability[day][person] == 2)) or (curr % 2 == 1 and (availability[day][person] == 1)):
+                        if (curr % 2 == 0 and (availability[day][person] == 2)) or \
+                                (curr % 2 == 1 and (availability[day][person] == 1)):
                             # Ignore the path
                             continue
 
@@ -248,10 +474,9 @@ def ford_fulkerson(availability, network, source, sink, n):
             # Move backwards
             curr = parent[curr]
 
-    print(network)
-    print(max_flow)
+    print(breakfast, dinner)
 
-    return breakfast, dinner
+    return breakfast, dinner, max_flow
 
 
 # Testing task 1
@@ -271,15 +496,15 @@ network2 = [[0, 4, 3, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0]]
 
 availability1 = [[2, 0, 2, 1, 2],
-                [3, 3, 1, 0, 0],
-                [0, 1, 0, 3, 0],
-                [0, 0, 2, 0, 3],
-                [1, 0, 0, 2, 1],
-                [0, 0, 3, 0, 2],
-                [0, 2, 0, 1, 0],
-                [1, 3, 3, 2, 0],
-                [0, 0, 1, 2, 1],
-                [2, 0, 0, 3, 0]]
+                 [3, 3, 1, 0, 0],
+                 [0, 1, 0, 3, 0],
+                 [0, 0, 2, 0, 3],
+                 [1, 0, 0, 2, 1],
+                 [0, 0, 3, 0, 2],
+                 [0, 2, 0, 1, 0],
+                 [1, 3, 3, 2, 0],
+                 [0, 0, 1, 2, 1],
+                 [2, 0, 0, 3, 0]]
 
 print(allocate(availability1))
 
