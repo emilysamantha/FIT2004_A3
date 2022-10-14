95b8bb3e24062c8577da462af651af8e6aefb085
emilysamantha <emilyzarry@gmail.com>
2022-10-14 18:07:19 +1100

    Implement so that each person only takes one meal each day and add function documentation

--- assignment3.py
+++ assignment3.py
@@ -10,8 +10,44 @@ from collections import deque
 
 # Task 1 - Sharing Meals
 def allocate(availability):
+    """
+    Function to determine allocation for preparing breakfast and dinner between 5 roommates (numbered 0, 1, 2, 3, 4)
+    in the next n days (numbered 0, 1, ..., n - 1). Ideally divided such that each meal is assigned to one person
+    and each person is assigned exactly 2n/5 meals. Allocation is done based on each person's availability.
+
+    However, a perfect allocation might not be possible. So a meal can be ordered from a restaurant if needed (the
+    number of meals that are ordered should not exceed 0.1n) and each person should be allocated to at least 0.36n
+    meals and at most 0.44n meals. No person should be allocated to both meals of the same day.
+
+    :Input:
+        availability: list of lists that contains data about the time availability of each person.
+                      For each day numbered i and each person numbered j, availability[i][j] is:
+                      - 0, if person j is not able to prepare both breakfast and dinner on day i
+                      - 1, if person j is only able to prepare breakfast on day i
+                      - 2, if person j is only able to prepare dinner on day i
+                      - 3, if person j is able to prepare both breakfast and dinner on day i
+
+    :Output:
+        - None, if an allocation that satisfies all constraints does not exist.
+        - (breakfast, dinner), where lists breakfast and dinner each specify a valid allocation.
+          breakfast[i] = j, if person j is allocated to prepare breakfast on day i
+          breakfast[i] = 5, if breakfast on day i will be ordered from a restaurant
+          dinner[i] = j, if person j is allocated to prepare dinner on day i
+          dinner[i] = 5, if dinner on day i will be ordered from a restaurant
+
+    :Time Complexity: Needs to be O(n^2), where n is the number of days
+    :Aux Space Complexity: Needs to be O(n^2), where n is the number of days
+
+    :Approach:
+    """
     graph = generate_network(availability)
-    return ford_fulkerson(graph, 0, len(graph) - 1, len(availability))
+    breakfast, dinner = ford_fulkerson(availability, graph, 0, len(graph) - 1, len(availability))
+
+    # If a day cannot be filled, then there is no valid allocation and return None
+    if -1 in breakfast or -1 in dinner:
+        return None
+
+    return breakfast, dinner
 
 
 def generate_network(availability):
@@ -47,9 +83,9 @@ def generate_network(availability):
     # Edge from meals to order option
     graph[1][7] = max_order
 
-    # Edge from order option to every day
-    for day in range(n):
-        graph[order_option_node][day + start_day_nodes] = 1
+    # Edge from order option to every meal
+    for meal in range(n * 2):
+        graph[order_option_node][meal + start_meal_nodes] = 1
 
     # Edge from each person to each day they can prepare a meal
     # and edge from each day to each meal
@@ -82,11 +118,15 @@ def generate_network(availability):
     # Edge from each meal to super sink node
     for i in range(n * 2):
         graph[i + start_meal_nodes][num_nodes - 1] = 1
-    print(graph)
     return graph
 
 
-def bfs(network, source, sink, parent):
+def bfs(availability, network, source, sink, parent, n):
+    # NODE INFORMATION
+    start_person_nodes = 2
+    start_day_nodes = 8
+    start_meal_nodes = start_day_nodes + n
+
     # Array to keep track of which nodes have been visited
     # Initialize all nodes as not visited
     visited = [False] * len(network)
@@ -109,6 +149,21 @@ def bfs(network, source, sink, parent):
         for i in range(len(network[curr])):
             # If an adjacent node of curr has not been visited
             if network[curr][i] > 0 and not visited[i]:
+
+                # If curr is a meal node
+                if start_meal_nodes <= curr < start_meal_nodes + (n * 2):
+                    # Check if the person is available for the meal
+                    day = parent[curr] - start_day_nodes
+                    person = parent[parent[curr]] - start_person_nodes
+
+                    # If the person is not the order option node
+                    if person != 5:
+                        # If curr is a breakfast node and the person can only prepare dinner or
+                        # if curr is a dinner node and the person can only prepare breakfast
+                        if (curr % 2 == 0 and (availability[day][person] == 2)) or (curr % 2 == 1 and (availability[day][person] == 1)):
+                            # Ignore the path
+                            continue
+
                 # Append to the queue
                 queue.append(i)
                 # Mark as visited
@@ -125,7 +180,7 @@ def bfs(network, source, sink, parent):
     return False
 
 
-def ford_fulkerson(network, source, sink, n):
+def ford_fulkerson(availability, network, source, sink, n):
     """
 
     :Input:
@@ -150,22 +205,32 @@ def ford_fulkerson(network, source, sink, n):
     dinner = [-1] * n
 
     # While there is an augmenting path in the network
-    while bfs(network, source, sink, parent):
+    while bfs(availability, network, source, sink, parent, n):
         # Starting from the sink node and going backwards until source
         curr = sink
         while curr != source:
             # If curr is a meal node
             if start_meal_nodes <= curr <= len(network) - 2:
-                # Record that the person will be preparing the corresponding meal
-                day = parent[curr] - start_day_nodes
-                person = parent[parent[curr]] - start_person_nodes
-
-                # If curr is even, it is a breakfast node
-                if curr % 2 == 0:
-                    breakfast[day] = person
-                # If curr is odd, it is a dinner node
+                # If the parent of the meal is the order option node
+                if parent[curr] == order_option_node:
+                    day = (curr - start_meal_nodes) // 2
+                    # If curr is even, it is a breakfast node
+                    if curr % 2 == 0:
+                        breakfast[day] = 5
+                    # If curr is odd, it is a dinner node
+                    else:
+                        dinner[day] = 5
                 else:
-                    dinner[day] = person
+                    # Record that the person will be preparing the corresponding meal
+                    day = parent[curr] - start_day_nodes
+                    person = parent[parent[curr]] - start_person_nodes
+
+                    # If curr is even, it is a breakfast node
+                    if curr % 2 == 0:
+                        breakfast[day] = person
+                    # If curr is odd, it is a dinner node
+                    else:
+                        dinner[day] = person
 
             # Move backwards
             curr = parent[curr]
@@ -182,6 +247,9 @@ def ford_fulkerson(network, source, sink, n):
 
             # Move backwards
             curr = parent[curr]
+
+    print(network)
+    print(max_flow)
 
     return breakfast, dinner
 
@@ -215,4 +283,28 @@ availability1 = [[2, 0, 2, 1, 2],
 
 print(allocate(availability1))
 
-# Task 2
+
+# Task 2 - Similarity Detector
+def compare_subs(submission1, submission2):
+    """
+    Function that uses a retrieval data structure to compare two submissions ang
+    determine their similarity.
+
+    :Input:
+        submission1: first string to compare containing characters in the range [a-z] or space
+        submission2: second string to compare containing characters in the range [a-z] or space
+
+    :Output:
+        res: list of findings with three elements:
+            - the longest common substring between submission1 and submission2
+            - the similarity score for submission1 (expressed as the percentage of submission1
+              that belongs to the longest common substring, rounded to the nearest integer)
+            - the similarity score for submission2 (expressed as the percentage of submission2
+              that belongs to the longest common substring, rounded to the nearest integer)
+
+    :Time Complexity: Needs to be O((N + M)^2)
+    :Aux Space Complexity: Needs to be O(N + M)
+
+    :Approach:
+    """
+    pass
